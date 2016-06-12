fff2bb1dfb327ee5f90c1179d5feae25782f41a2
Alex Litoiu <alex@9dotlabs.com>
2016-06-11 18:24:58 -0700

    [p001] Decreased number of functions in sol1

--- haskell/001/sol1.hs
+++ haskell/001/sol1.hs
@@ -5,11 +5,8 @@ limit = 1000
 multiplesOf :: Int -> [Int]
 multiplesOf value = takeWhile (< limit) [j * value | j <- [1..]]
 
-multiples :: [Int]
-multiples = union (multiplesOf 3) (multiplesOf 5)
-
 solution :: Int
-solution = sum multiples
+solution = sum $ union (multiplesOf 3) (multiplesOf 5)
 
 main :: IO ()
 main = print $ solution
